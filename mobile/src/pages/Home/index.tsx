9e562cc14fbae4d495729742eaf19d8218ea026a
Augusto <ahaavila@gmail.com>
2020-06-23 19:45:36 -0300

    Mobile
    Termino da criação da pagina home

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, ImageBackground, Image, StyleSheet, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { Feather as Icon } from '@expo/vector-icons';
 
 const Home = () => {
   return (
@@ -15,7 +17,16 @@ const Home = () => {
       </View>
 
       <View style={styles.footer}>
-        
+        <RectButton style={styles.button} onPress={() => {}}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#fff" size={24}></Icon>
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
+          </Text>
+        </RectButton>
       </View>
     </ImageBackground>
   )
