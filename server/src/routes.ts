b96ea7c64c23917650aa5fede05144fad485342c
Augusto <ahaavila@gmail.com>
2020-07-07 21:10:25 -0300

    Validação de entrada
    Adicionado validação de dados no backend com a biblioteca 'celebrate'.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
 import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
@@ -13,8 +14,27 @@ const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', upload.single('image') ,pointsController.create);
 routes.get('/points/:id', pointsController.show);
 routes.get('/points', pointsController.index);
+
+routes.post(
+  '/points', 
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false,
+  }), 
+  pointsController.create
+);
 
 export default routes;
